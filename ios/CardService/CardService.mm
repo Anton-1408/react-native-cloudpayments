6eba8f973cd2ee46b83b5a6f741fb7239ff9449f
Anton <antonvotinov@gmail.com>
2025-06-10 09:19:12 +0600

    feat: [ios] add CardService module

--- ios/CardService/CardService.mm
+++ ios/CardService/CardService.mm
@@ -8,8 +8,89 @@ RCT_EXPORT_MODULE()
     (NSString *)cardNumber
     resolve:(RCTPromiseResolveBlock)resolve
     reject:(RCTPromiseRejectBlock)reject {
-
   [[CardServiceSwift shared] cardType:cardNumber resolve:resolve reject:reject];
+}
+
+-(void)isValidNumber:
+  (NSString *)cardNumber
+  resolve:(RCTPromiseResolveBlock)resolve
+  reject:(RCTPromiseRejectBlock)reject {
+  [[CardServiceSwift shared] isValidNumber:cardNumber resolve:resolve reject:reject];
+}
+
+-(void)isValidExpDate:
+  (NSString *)expDate
+  resolve:(RCTPromiseResolveBlock)resolve
+  reject:(RCTPromiseRejectBlock)reject {
+  [[CardServiceSwift shared] isValidExpDate:expDate resolve:resolve reject:reject];
+}
+
+-(void)createCardCryptogram:
+  (NSString *)cardNumber
+  cardDate:(NSString *)cardDate
+  cardCVC: (NSString *)cardCVC
+  publicId: (NSString *)publicId
+  publicKey: (NSString *)publicKey
+  resolve:(RCTPromiseResolveBlock)resolve
+  reject:(RCTPromiseRejectBlock)reject{
+  [[CardServiceSwift shared]
+   createCardCryptogram: cardNumber
+   cardDate: cardDate
+   cardCVC: cardCVC
+   publicId: publicId
+   publicKey: publicKey
+   resolve: resolve
+   reject: reject
+  ];
+}
+
+-(void)cardCryptogramForCVV:
+  (NSString *)cvv
+  resolve:(RCTPromiseResolveBlock)resolve
+  reject:(RCTPromiseRejectBlock)reject {
+  [[CardServiceSwift shared] cardCryptogramForCVV:cvv resolve:resolve reject:reject];
+}
+
+-(void)isValidExpDateFull:
+  (NSString *)expDate
+  resolve:(RCTPromiseResolveBlock)resolve
+  reject:(RCTPromiseRejectBlock)reject {
+  [[CardServiceSwift shared] isValidExpDateFull:expDate resolve:resolve reject:reject];
+}
+
+-(void)isValidCvv:
+  (NSString *)cvv
+  resolve:(RCTPromiseResolveBlock)resolve
+  reject:(RCTPromiseRejectBlock)reject {
+  [[CardServiceSwift shared] isValidCvv:cvv resolve:resolve reject:reject];
+}
+
+-(void)createHexPacketFromData:
+  (NSString *)cardNumber
+  cardExp:(NSString *)cardExp
+  cardCvv: (NSString *)cardCvv
+  publicId: (NSString *)publicId
+  publicKey: (NSString *)publicKey
+  keyVersion: (NSInteger)keyVersion
+  resolve:(RCTPromiseResolveBlock)resolve
+  reject:(RCTPromiseRejectBlock)reject {
+  [[CardServiceSwift shared]
+   createHexPacketFromData: cardNumber
+   cardExp: cardExp
+   cardCvv: cardCvv
+   publicId: publicId
+   publicKey: publicKey
+   keyVersion: keyVersion
+   resolve: resolve
+   reject: reject
+  ];
+}
+
+-(void)createMirPayHexPacketFromCryptogram:
+  (NSString *)cryptogram
+  resolve:(RCTPromiseResolveBlock)resolve
+  reject:(RCTPromiseRejectBlock)reject {
+  [[CardServiceSwift shared] createMirPayHexPacketFromCryptogram:cryptogram resolve:resolve reject:reject];
 }
 
 
--- ios/CardService/CardService.swift
+++ ios/CardService/CardService.swift
@@ -1,5 +1,6 @@
 import Foundation
 import React
+import Cloudpayments
 
 @objc(CardServiceSwift)
 public class CardServiceSwift: NSObject {
@@ -12,5 +13,84 @@ public class CardServiceSwift: NSObject {
   }
   
   @objc
-  public func cardType(_ cardNumber: String, resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {}
+  public func cardType(_ cardNumber: String, resolve: RCTPromiseResolveBlock?, reject: RCTPromiseRejectBlock?) {
+    let cardType = Card.cardType(from: cardNumber)
+    resolve?(cardType.toString())
+  }
+  
+  @objc
+  public func isValidNumber(_ cardNumber: String, resolve: RCTPromiseResolveBlock?, reject: RCTPromiseRejectBlock?) {
+    let isValidNumber = Card.isCardNumberValid(cardNumber)
+    resolve?(isValidNumber)
+  }
+  
+  @objc
+  public func isValidExpDate(_ expDate: String, resolve: RCTPromiseResolveBlock?, reject: RCTPromiseRejectBlock?) {
+    let isValidExpDate = Card.isExpDateValid(expDate)
+    resolve?(isValidExpDate)
+  }
+  
+  @objc
+  public func createCardCryptogram(
+    _ cardNumber: String,
+    cardDate: String,
+    cardCVC: String,
+    publicId: String,
+    publicKey: String,
+    resolve: RCTPromiseResolveBlock?,
+    reject: RCTPromiseRejectBlock?
+  ) {
+    let publicKeyToInt = Int(publicKey) ?? 0
+    
+    let cardCryptogramPacket = Card.makeCardCryptogramPacket(cardNumber: cardNumber, expDate: cardDate, cvv: cardCVC, merchantPublicID: publicId, publicKey: publicKey, keyVersion: publicKeyToInt)
+    
+    if (cardCryptogramPacket != nil) {
+      resolve?(cardCryptogramPacket)
+    } else {
+      reject?("error", "cvvCryptogramPacket is undefined", nil);
+    }
+  }
+  
+  @objc
+  public func cardCryptogramForCVV(
+    _ cvv: String,
+    resolve: RCTPromiseResolveBlock?,
+    reject: RCTPromiseRejectBlock?) {
+      let cardCryptogramPacket = Card.makeCardCryptogramPacket(with: cvv)
+      
+      if (cardCryptogramPacket != nil) {
+        resolve?(cardCryptogramPacket)
+      } else {
+        reject?("error", "cvvCryptogramPacket is undefined", nil);
+      }
+  }
+  
+  @objc
+  public func isValidExpDateFull(_ expDate: String, resolve: RCTPromiseResolveBlock?, reject: RCTPromiseRejectBlock?) {
+    reject?("error", "method not yet available", nil);
+  }
+  
+  @objc
+  public func isValidCvv(_ cvv: String, resolve: RCTPromiseResolveBlock?, reject: RCTPromiseRejectBlock?) {
+    let isValidCvv = Card.isValidCvv(cvv: cvv)
+    resolve?(isValidCvv)
+  }
+  
+  @objc
+  public func createHexPacketFromData(
+    _ cardNumber: String,
+    cardExp: String,
+    cardCvv: String,
+    publicId: String,
+    publicKey: String,
+    keyVersion: Int,
+    resolve: RCTPromiseResolveBlock?,
+    reject: RCTPromiseRejectBlock?) {
+      reject?("error", "method not yet available", nil);
+  }
+  
+  @objc
+  public func createMirPayHexPacketFromCryptogram(_ cryptogram: String, resolve: RCTPromiseResolveBlock?, reject: RCTPromiseRejectBlock?) {
+    reject?("error", "method not yet available", nil);
+  }
 }
